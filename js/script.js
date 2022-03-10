e82751696a4268279d7310c1f186432df79d3571
alutton <andrew.lutton@gmail.com>
2022-03-10 10:58:17 -0500

    Add getRandomInt() and computerPlay()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("Link works")
+function computerPlay() {
+    let playInt = getRandomInt(1, 3);
+    return(playInt);
+}
+
+//Get Random Integer between two numbers, inclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() *(max - min + 1) + min);
+}
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
