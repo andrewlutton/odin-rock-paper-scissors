9c4050ef314d903a44e26123c35e3af22a23239e
alutton <andrew.lutton@gmail.com>
2022-03-14 10:49:07 -0400

    Add gui

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
+const playBtns = document.querySelectorAll(".play-btn");
+playBtns.forEach(btn => btn.addEventListener("click", playRound));
+
+const scoreReport = document.querySelector(".message-container");
+
+let compScore = 0;
+let playerScore = 0;
+
+const resultsMsg = document.querySelector("#results-message");
+const playerChoice = document.querySelector("#player-choice");
+const computerChoice = document.querySelector("#computer-choice");
+
+const compScoreReport = document.createElement("p");
+const playerScoreReport = document.createElement("p");
+
+compScoreReport.textContent = "Pink Robots: " + compScore;
+playerScoreReport.textContent = "Yoshimi: " + playerScore;
+
+scoreReport.appendChild(compScoreReport);
+scoreReport.appendChild(playerScoreReport);
+
 // Computer plays randomly
 function computerPlay() {
     switch (getRandomInt(1, 3)) {
         case 1:
-            play = "Rock";
+            play = "rock";
             break;
         case 2:
-            play = "Paper";
+            play = "paper";
             break;
         case 3:
-            play = "Scissors";
+            play = "scissors";
             break;
     }
 
@@ -23,75 +44,80 @@ function getRandomInt(min, max) {
 }
 
 // Evaluate Game Result: return ["winner", "message"]
-function roundEval(roundNum, playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        result = ["draw", "Round " + roundNum + ": Draw!"];
-    } else if (
-        playerSelection === "Rock" && computerSelection === "Scissors" ||
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper") {
+function playRound() {
+    const computerSelection = computerPlay();
+    const playerSelection = this.id;
 
-        result = ["player", "Round " + roundNum + ": You win the round! " + playerSelection + " beats " + computerSelection];
+    playerChoice.textContent = "You chose: " + this.id;
+    computerChoice.textContent = "Pink Robots chose: " + computerSelection;
+
+    if (playerSelection === computerSelection) {
+        resultsMsg.textContent = "Draw! The universe will have it's way. Try again.";
+    } else if (
+        playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+            resultsMsg.textContent = "You've clearly taken lots of vitamins. I know you can beat them. ";
+            playerScore += 1;
+            playerScoreReport.textContent = "Yoshimi: " + playerScore;
     } else {
-        result = ["computer", "Round " + roundNum + ": You lose the round! " + computerSelection + " beats " + playerSelection];
+        resultsMsg.textContent = "You have to discipline your body. Those evil-natured robots are programmed to destroy us. You've got to be strong to fight them. Try again.";
+        compScore += 1;
+        compScoreReport.textContent = "Pink Robots: " + compScore;
     }
-    return (result);
+    checkVictory();
+}
+
+function checkVictory() {
+    if (playerScore >= 5) {
+        resultsMsg.textContent = "The Pink Robots have been defeated! Thank you for your service to the city!"
+    } else if (compScore >= 5) {
+        resultsMsg.textContent = "Those evil robots won. Now they're going to eat us."
+    }
 }
 
 //Get player selection
-function playerPlay() {
-    let playerString = "";
-    while (playerString === "") {
+// function playerPlay() {
+//     let playerString = "";
+//     while (playerString === "") {
 
-        let str = prompt("Select Rock, Paper, or Scissors:");
-        str = str.toLowerCase();
-        str = str.charAt(0).toUpperCase() + str.slice(1);
+//         let str = prompt("Select Rock, Paper, or Scissors:");
+//         str = str.toLowerCase();
+//         str = str.charAt(0).toUpperCase() + str.slice(1);
 
-        if (str === "Rock" || str === "Paper" || str === "Scissors") {
-            playerString = str;
-        } else {
-            console.log("Player must choose rock, paper, or scissors. Please try again.");
-        }
-    }
-    return (playerString);
-}
+//         if (str === "Rock" || str === "Paper" || str === "Scissors") {
+//             playerString = str;
+//         } else {
+//             console.log("Player must choose rock, paper, or scissors. Please try again.");
+//         }
+//     }
+//     return (playerString);
+// }
 
-//Handle single round
-function playRound(roundNum) {
-    let computerSelection = computerPlay();
-    let playerSelection = playerPlay();
-    let results = roundEval(roundNum, playerSelection, computerSelection);
-    let winner = results[0];
-    console.log(results[1]);
-    return(winner);
-}
-
-function game() {
+// function game() {
     
-    let playerScore = 0
-    let computerScore = 0
+//     let playerScore = 0
+//     let computerScore = 0
     
-    for (let i = 0; i < 5; i++) {
-        switch (playRound(i + 1)) {
-            case "player":
-                playerScore++;
-                break;
-            case "computer":
-                computerScore++;
-                break;
-            case "draw":
-                break;
-        };
-    }
-    console.log("Your score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-    if (playerScore > computerScore) {
-        console.log("You win!");
-    } else if ( playerScore < computerScore) {
-        console.log("You lose!");
-    } else {
-        console.log("Draw!")
-    }
-}
-
-game();
+//     for (let i = 0; i < 5; i++) {
+//         switch (playRound(i + 1)) {
+//             case "player":
+//                 playerScore++;
+//                 break;
+//             case "computer":
+//                 computerScore++;
+//                 break;
+//             case "draw":
+//                 break;
+//         };
+//     }
+//     console.log("Your score: " + playerScore);
+//     console.log("Computer score: " + computerScore);
+//     if (playerScore > computerScore) {
+//         console.log("You win!");
+//     } else if ( playerScore < computerScore) {
+//         console.log("You lose!");
+//     } else {
+//         console.log("Draw!")
+//     }
+// }
